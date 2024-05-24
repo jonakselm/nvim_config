0daa9dec91bf9c73cf678744c37ddbbcb829d679
Jon Aksel <jonakselm@gmail.com>
2024-05-25 01:38:10 +0200

    Removed unused stuff

--- init.lua
+++ init.lua
@@ -1,22 +1,5 @@
 vim.cmd(
 [[call plug#begin()
-		
-" lsp
-Plug 'prabirshrestha/vim-lsp'
-
-" autocomplete
-Plug 'prabirshrestha/asyncomplete.vim'
-Plug 'prabirshrestha/asyncomplete-lsp.vim'
-Plug 'mfussenegger/nvim-lsp-compl'
-
-" highlighting
-Plug 'jackguo380/vim-lsp-cxx-highlight'
-
-" snippets
-Plug 'hrsh7th/vim-vsnip'
-Plug 'hrsh7th/vim-vsnip-integ'
-
-" Function signatures
 Plug 'ray-x/lsp_signature.nvim'
 
 call plug#end()
@@ -48,36 +31,10 @@ vim.api.nvim_create_autocmd('LspAttach',
 	end
 })
 
---vim.cmd([[
---if executable('clangd')
---    augroup lsp_clangd
---        autocmd!
---        autocmd User lsp_setup call lsp#register_server({
---                    \ 'name': 'clangd',
---                    \ 'cmd': {server_info->['clangd']},
---                    \ 'initialization_options': {
---                    \   'highlight': { 'lsRanges' : v:true },
---                    \ },
---                    \ 'whitelist': ['c', 'cpp', 'objc', 'objcpp'],
---                    \ })
---        autocmd FileType c setlocal omnifunc=lsp#complete
---        autocmd FileType cpp setlocal omnifunc=lsp#complete
---        autocmd FileType objc setlocal omnifunc=lsp#complete
---        autocmd FileType objcpp setlocal omnifunc=lsp#complete
---    augroup end
---endif
---]])
 
 vim.cmd([[
 	" Goes through the option list if there is one, else tab
 	inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-	
-
-	" Quality of life changes
-	" inoremap jj <ESC>
-	" set tabstop=4
-	" set shiftwidth=4
-	" set relativenumber
 ]])
 
 vim.keymap.set('i', 'jj', '<esc>')
@@ -87,6 +44,3 @@ vim.opt.relativenumber = true
 
 
 vim.api.nvim_set_hl(0, 'LineNr', { fg = "yellow" })
---"let g:lsp_semantic_enabled = 1
---"let g:lsp_cxx_hl_use_text_props = 1
---hi Comment ctermfg=DarkGreen
